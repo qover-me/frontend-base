32edbda941c37f04a7ce3e91d540685e27ac21aa
Juan Carlos Sanchez Cardenas <juan@qover.me>
2019-09-18 22:43:40 +0200

    comment gcloud script pending service account scope change

--- scripts/gcloud-get-credentials.sh
+++ scripts/gcloud-get-credentials.sh
@@ -3,5 +3,5 @@ set -e
 # Write GCP service account private key into a file
 echo $GCLOUD_SERVICE_KEY > $HOME/gcloud-service-key.json
 
-gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
-gcloud container clusters get-credentials $CREDENTIALS_ENV --zone europe-west1-b --project $GCLOUD_PROJECT_ID
+# gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
+# gcloud container clusters get-credentials $CREDENTIALS_ENV --zone europe-west1-b --project $GCLOUD_PROJECT_ID
