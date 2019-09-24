37ab8f10fddc06be4f34ecff3cfa9d6f8826c2b1
Juan Carlos Sanchez Cardenas <juan@qover.me>
2019-09-24 19:52:37 +0200

    comment auth line

--- scripts/gcloud-get-credentials.sh
+++ scripts/gcloud-get-credentials.sh
@@ -5,5 +5,5 @@ echo $GCLOUD_SERVICE_KEY > $HOME/gcloud-service-key.json
 
 echo "credentials env: $CREDENTIALS_ENV" 
 
-gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
+# gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
 gcloud container clusters get-credentials $CREDENTIALS_ENV --zone europe-west1-b --project $GCLOUD_PROJECT_ID
