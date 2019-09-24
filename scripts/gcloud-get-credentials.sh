af1c00dce2081d8bb52ee0a468e5a65accd52d33
Juan Carlos Sanchez Cardenas <juan@qover.me>
2019-09-24 19:16:06 +0200

    reset gcloud auth

--- scripts/gcloud-get-credentials.sh
+++ scripts/gcloud-get-credentials.sh
@@ -3,5 +3,7 @@ set -e
 # Write GCP service account private key into a file
 echo $GCLOUD_SERVICE_KEY > $HOME/gcloud-service-key.json
 
-# gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
-# gcloud container clusters get-credentials $CREDENTIALS_ENV --zone europe-west1-b --project $GCLOUD_PROJECT_ID
+echo "credentials env: $CREDENTIALS_ENV" 
+
+gcloud auth activate-service-account --key-file $HOME/gcloud-service-key.json
+gcloud container clusters get-credentials $CREDENTIALS_ENV --zone europe-west1-b --project $GCLOUD_PROJECT_ID
